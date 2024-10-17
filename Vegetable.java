ef04bb8c38a4c23413b63ee1ff06049bca6aca8c
Sherif Elglaly <Sherifelglaly@gmail.com>
2024-10-17 14:44:54 -0700

    Add files via upload

---
+++ Vegetable.java
@@ -0,0 +1,42 @@
+package com.gradescope.garden;
+
+public class Vegetable extends Plant {
+
+	
+
+	private boolean isHarvest = false;
+	
+	
+	public Vegetable(String type, int age, String color, int growthRate, Health health, boolean isHarvest) {
+		super(type, age, color, growthRate, health);
+		this.isHarvest=isHarvest;
+		// TODO Auto-generated constructor stub
+	}
+
+
+	/**
+	 * @return the isHarvest
+	 */
+	public boolean getHarvest() {
+		return isHarvest;
+	}
+
+
+	/**
+	 * @param isHarvest the isHarvest to set
+	 */
+	public void setHarvest(boolean isHarvest) {
+		this.isHarvest = isHarvest;
+		
+	}
+	
+	public void harvest()
+	{
+		setHarvest(true);
+		this.setGrowthRate(0);
+	}
+	
+	
+	
+	
+}
